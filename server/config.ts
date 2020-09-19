f5346de87eab3b3e4613bfc887cb636f72a76e78
Gem Xli <x.li.gem@gmail.com>
2020-09-19 23:04:41 +0800

    feat: set cache

--- server/config.ts
+++ server/config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 const resolve = (...args: string[]) => path.join(__dirname, '..', ...args)
 
 export const config = {
+  isDev: process.env.NODE_ENV === 'development',
   assetsDir: resolve('data'),
   htmlDir: resolve('public'),
   filter(fileName: string, filePath: string) {
--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,16 @@ import { API } from './api'
 import { AssetsResources } from './assets'
 import { config } from './config'
 import { PublicAssets } from './public'
+import koaCash from 'koa-cash'
+import LRU from 'lru-cache'
+
+const debug = require('debug')('fbrowser:app')
+
+const cacheMaxAge = 3600 * 12
+
+const cache = new LRU({
+  maxAge: cacheMaxAge
+})
 
 const app = new Koa()
 
@@ -14,10 +24,35 @@ app
     }
 
     await next()
+
+    if (config.isDev) {
+      ctx.set('Access-Control-Allow-Origin', '*')
+    }
+  })
+  .use(API())
+  .use(
+    koaCash({
+      maxAge: cacheMaxAge,
+      setCachedHeader: true,
+      compression: true,
+      async get(key) {
+        return cache.get(key)
+      },
+      async set(key, value) {
+        cache.set(key, value)
+      }
+    })
+  )
+  .use(async (ctx, next) => {
+    const isCached = await ctx.cashed()
+    debug(`is cached: ${isCached}`)
+
+    if (!isCached) {
+      await next()
+    }
   })
   .use(PublicAssets(config.htmlDir))
   .use(AssetsResources(config.assetsDir))
-  .use(API())
 
 app.listen(3000, () => {
   console.log(`server listen(${process.env.NODE_ENV}): http://localhost:3000`)
