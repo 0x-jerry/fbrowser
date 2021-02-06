28714cb509cd6bb173e1a758956dda66b9386a00
Gem Xli <x.li.gem@gmail.com>
2021-02-06 17:03:34 +0800

    更新测试文件

--- client/components/Explore.vue
+++ client/components/Explore.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="explore">
-    <div class="explore-route">
+    <div class="explore-route px-3 py-2 border-gray-300 border-b">
       路径:
       <span class="current-path text-blue-500">{{ currentPath }}</span>
     </div>
-    <div class="explore-files">
-      <div class="explore-file text-4xl">
-        <f-icon name="image" />
-        <f-icon name="video" />
-        <f-icon name="music" />
-        <f-icon name="folder" />
-        <f-icon name="unknown" />
-        xxx
+    <div class="explore-files p-2">
+      <div class="explore-file text-2xl" v-for="file in files" :key="file.name">
+        <f-icon v-if="file.folder" class="mr-2" name="folder" @click="openFolder(file.name)" />
+        <f-icon v-else class="mr-2" :name="calcType(file.type)" />
+        <span>{{ file.name }}</span>
       </div>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { api } from '../api'
 import FIcon from './FIcon.vue'
 
@@ -38,7 +35,15 @@ export default {
   methods: {
     async getFiles() {
       const data = await api.getFiles(this.dir)
-      console.log(data)
+      this.files = data.files
+    },
+    calcType(type: string) {
+      return type.split('/').shift()
+    },
+    async openFolder(name: string) {
+      this.dir = this.dir + '/' + name
+      const data = await api.getFiles(this.dir)
+      this.files = data.files
     }
   }
 }
@@ -47,7 +52,6 @@ export default {
 <style lang="less" scoped>
 .explore {
   &-route {
-    padding: 5px;
   }
 }
 </style>
--- client/components/FIcon.vue
+++ client/components/FIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <i :class="['iconfont', iconClass, 'iconfile-' + name]"> </i>
+  <i :class="['iconfont', iconClass, 'iconfile-' + nameClass]"> </i>
 </template>
 
 <script lang="ts">
@@ -7,7 +7,7 @@ const colorMap = {
   image: 'text-red-500',
   folder: 'text-yellow-500',
   video: 'text-purple-500',
-  music: 'text-green-500',
+  audio: 'text-green-500',
   unknown: 'text-gray-500'
 }
 
@@ -16,8 +16,15 @@ export default {
     name: String
   },
   computed: {
+    nameClass() {
+      if (colorMap[this.$props.name]) {
+        return this.$props.name
+      }
+
+      return 'unknown'
+    },
     iconClass() {
-      return colorMap[this.$props.name]
+      return colorMap[this.nameClass]
     }
   }
 }
