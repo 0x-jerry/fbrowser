7f86b7959f5fd39d55a28d8441cc8e2ac83628c7
Gem Xli <x.li.gem@gmail.com>
2021-02-06 21:23:22 +0800

    测试

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,9 +2,11 @@ import path from 'path'
 
 const resolve = (...args: string[]) => path.join(__dirname, '..', ...args)
 
+const isDev = process.env.NODE_ENV === 'development'
+
 export const config = {
-  isDev: process.env.NODE_ENV === 'development',
-  assetsDir: resolve('data'),
+  isDev: isDev,
+  assetsDir: isDev ? resolve('test-data') : resolve('data'),
   htmlDir: resolve('public'),
   filter(fileName: string, filePath: string) {
     return !fileName.startsWith('.')
